26f2e6056c7f0561b757774f400a2c0f933f3525
Alex <alexdavi150@gmail.com>
2021-12-29 10:55:38 -0300

    redirecting user to the product list page

--- index.php
+++ index.php
@@ -1,8 +1,14 @@
 <?php
 
-$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+  require_once 'config.php';
 
-print_r($url);
-echo substr($url['path'], 1);
+  header('Access-Control-Allow-Origin: *');
+  // header('Content-Type: application/json');
+  header("Access-Control-Allow-Methods: HEAD, GET, POST, PUT, PATCH, DELETE, OPTIONS");
+  header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method,Access-Control-Request-Headers, Authorization");
+
+  if($_SERVER['REQUEST_METHOD'] == 'GET') {
+    header('location: '.FE_URL);
+  }
 
 ?>
