ba090c1f0393659397ab86772f94fa47328d26bd
Alex <alexdavi150@gmail.com>
2022-01-10 23:25:10 -0300

    changed global variables to work on local machine

--- config.php
+++ config.php
@@ -1,7 +1,10 @@
 <?php
 
   // Define global variables
-  define('BE_URL', 'http://pimentatestando.com');
-  define('FE_URL', 'http://pimentatestando.com:3000');
+  define('BE_URL', 'http://localhost/PHP/scandiweb-test');
+  define('FE_URL', 'http://localhost:3000');
+  
+  // define('BE_URL', 'http://pimentatestando.com');
+  // define('FE_URL', 'http://pimentatestando.com:3000');
 
 ?>
